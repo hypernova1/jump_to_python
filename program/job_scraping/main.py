8d889c2f32b3ccc6b3aa2cea4d1d8e850c357770
hypernova1 <chtlstjd01@gmail.com>
2020-09-17 21:58:18 +0900

    회사 정보 딕셔너리로 생성

--- program/job_scraping/main.py
+++ program/job_scraping/main.py
@@ -2,26 +2,44 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-page = 1
-url = f"https://www.rocketpunch.com/api/jobs/template?page={page}"
+URL = f"https://www.rocketpunch.com/api/jobs/template?location=서울특별시&specialty=Java&specialty=JSP&tag=웹서비스&tag=e-commerce"
 
-req = requests.get(url)
 
-st_json = json.dumps(req.text)
+def parse_total_page():
+    response = requests.get(URL)
+    json_dumps = json.dumps(response.text)
+    json_data = json.loads(json_dumps)
+    json_data = json.loads(json_data)
+    body = json_data["data"]["template"]
+    soup = BeautifulSoup(body, "html.parser")
+    pagination = soup.find("div", {"class": "pagination"})
+    pagination = pagination.find("div", {"class", "widescreen"})
+    return int(pagination.find_all("a", {"class": "item"})[-1].string)
 
-dict = json.loads(st_json)
-dict = json.loads(dict)
-html = dict["data"]["template"]
 
-soup = BeautifulSoup(html, "html.parser")
+total_page = parse_total_page()
 
-body = soup.find("div", {"id": "company-list"})
 
-companies = body.find_all("div", {"class": "company"})
-for company in companies:
+def extract_company_info(company):
     company_id = company["data-company_id"]
     company_name = company.find("div", {"class": "company-name"})
     company_name = company_name.find("h4", {"class": "header"})
     company_name = company_name.find("strong").string
-    # print(job.find("h4", {"class", "header"}))
+    service_types = company.find("div", {"class": "nowrap"}).get_text(strip=True)
+    company_url = company.find("a")["href"]
+    company_url = f"https://www.rocketpunch.com/{company_url}"
+    return {'id': company_id, 'compnay_name': company_name, 'service_type': service_types, 'company_url': company_url}
 
+
+for page in range(total_page):
+    req = requests.get(f"{URL}&page={page + 1}")
+    st_json = json.dumps(req.text)
+    dict = json.loads(st_json)
+    dict = json.loads(dict)
+    html = dict["data"]["template"]
+
+    soup = BeautifulSoup(html, "html.parser")
+    body = soup.find("div", {"id": "company-list"})
+    companies = body.find_all("div", {"class": "company"})
+    for company in companies:
+        company_info = extract_company_info(company)
