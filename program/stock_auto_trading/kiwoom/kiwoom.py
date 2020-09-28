16fe21e66eb09164fb421a535f7cb5bb4f1bb9ee
hypernova1 <hypemova@gmail.com>
2020-09-29 01:17:01 +0900

    예수금 가져오기

--- program/stock_auto_trading/kiwoom/kiwoom.py
+++ program/stock_auto_trading/kiwoom/kiwoom.py
@@ -10,22 +10,34 @@ class Kiwoom(QAxWidget):
 
         self.login_event_loop = QEventLoop()
 
+        self.account_num = None
+        self.deposit = 0
+        self.use_money = 0
+        self.use_money_percent = 0.5
+        self.output_deposit = 0
+
+        self.screen_my_info = "2000"
+
         self.get_ocx_instance()
         self.event_slots()
         self.signal_login_commConnect()
         self.get_account_info()
+        self.detail_account_info()
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 
+    def event_slots(self):
+        self.OnEventConnect.connect(self.login_slot)
+        self.OnReceiveTrData.connect(self.trdata_slot)
+
     def signal_login_commConnect(self):
         self.dynamicCall("CommConnect()")
+        self.login_event_loop.exec_() # 로그인을 요청하는 송/수신이 안전하게 완료될 때 까지 대기
 
     def login_slot(self, err_code):
         print(errors(err_code)[1])
-
-    def event_slots(self):
-        self.OnEventConnect.connect(self.login_slot)
+        self.login_event_loop.exit() # 로그인이 완료되면 QEventLoop가 종료되고 다음 코드 실행
 
     def get_account_info(self):
         account_list = self.dynamicCall("GetLoginInfo(QString)", "ACCNO")
@@ -35,3 +47,28 @@ class Kiwoom(QAxWidget):
 
         print("계좌번호: %s" % account_num)
 
+    def detail_account_info(self, sPrevNext="0"):
+        self.dynamicCall("SetInputValue(QString, QString)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(QString, QString)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(QString, QString)", "비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(QString, QString)", "조회구분", "1")
+        self.dynamicCall("CommRqData(QString, QString, int, QString)",
+                         "예수금상세현황요청", "opw00001", sPrevNext, self.screen_my_info)
+
+    def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
+        if sRQName == "예수금상세현황요청":
+            deposit = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, 0, "예수금")
+            self.deposit = int(deposit)
+
+            use_money = float(self.deposit) * self.use_money_percent
+            self.use_money = int(use_money)
+            self.use_money = self.use_money / 4
+
+            output_deposit = self.dynamicCall("GetCommData(QString, QString, int, QString",
+                                              sTrCode, sRQName, 0, "출금가능금액")
+            self.output_deposit = int(output_deposit)
+            print("예수금: %s" % self.output_deposit)
+            self.stop_screen_cancel(self.screen_my_info)
+
+    def stop_screen_cancel(self, sScrNo=None):
+        self.dynamicCall("DisconnectRealData(QString)", sScrNo)
