6b9196ae411d88e7f79e9abf2fafaeb896705b61
hypernova1 <hypemova@gmail.com>
2020-09-28 23:09:07 +0900

    계좌번호 가져오기

--- program/stock_auto_trading/kiwoom/kiwoom.py
+++ program/stock_auto_trading/kiwoom/kiwoom.py
@@ -11,19 +11,27 @@ class Kiwoom(QAxWidget):
         self.login_event_loop = QEventLoop()
 
         self.get_ocx_instance()
-        self.event_slot()
+        self.event_slots()
         self.signal_login_commConnect()
+        self.get_account_info()
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 
     def signal_login_commConnect(self):
         self.dynamicCall("CommConnect()")
-        self.login_event_loop.exec_()
 
     def login_slot(self, err_code):
         print(errors(err_code)[1])
-        self.login_event_loop.exit()
 
-    def event_slot(self):
+    def event_slots(self):
         self.OnEventConnect.connect(self.login_slot)
+
+    def get_account_info(self):
+        account_list = self.dynamicCall("GetLoginInfo(QString)", "ACCNO")
+        account_num = account_list.split(';')[0]
+
+        self.account_num = account_num
+
+        print("계좌번호: %s" % account_num)
+
